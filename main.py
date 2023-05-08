8a57794d163065c1954c26cfa63fdca8c22f1eef
alekg <alekg@mail.fff>
2023-05-08 15:20:42 +0300

    add implementation of Task5

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import Tasks.Task1
 import Tasks.Task2
 import Tasks.Task3
 import Tasks.Task4
+import Tasks.Task5
 
 def main():
     Tasks.Task1.task1()
     Tasks.Task2.task2()
     Tasks.Task3.task3()
     Tasks.Task4.task4()
+    Tasks.Task5.task5()
 
 if __name__ == "__main__":
     main()
